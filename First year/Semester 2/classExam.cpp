7000ca722ee6bd8bf23abea79818b1444bf1da00
onyxpeek <onyxpeek@gmail.com>
2023-03-30 13:35:31 +0300

    'feat: used also a method for setting all the data for an object at the same time

--- First year/Semester 2/classExam.cpp
+++ First year/Semester 2/classExam.cpp
@@ -13,7 +13,7 @@ private://Avem 4 variabile membre private
     double pret;
 
 public:
-    //In public avem 5 metode publice
+    //In public avem 5 metode publice pentru setarea datelor in obiectul medicament 1 prin metoda 1
     void setNume(string numeMedicament) {
         nume = numeMedicament;
     }
@@ -30,11 +30,19 @@ public:
         pret = pretMedicament;
     }
 
-    void afiseazaDetalii() const {
+    //Aici am o metoda care imi afiseaza aceste date de la obiectul pe care am sa l selectez prin .metoda
+    void afiseazaDetalii()  {
         cout << "Nume medicament: " << nume << endl;
         cout << "Concentratie: " << concentr << endl;
         cout << "Substanta activa: " << subsAct << endl;
         cout << "Pret: " << pret << endl;
+    }
+
+    void setTot(string numeMedicament, int concentratie, string substantaActiva, double pretMedicament) {
+        nume = numeMedicament;
+        concentr = concentratie;
+        subsAct = substantaActiva;
+        pret = pretMedicament;
     }
 
 };
@@ -46,15 +54,17 @@ int main() {
 metode(datele sunt private si metodele sunt publice), 
 mai precis o metoda este o functie care poate face ceva anumit cu obiectul dat*/
 
-        Medicament medicament1;
+        Medicament medicament1, medicament2;
         medicament1.setNume("Paracetamol");
         medicament1.setConcentr(500);
-        medicament1.setSubsAct("Paracetamol");
+        medicament1.setSubsAct("Pasracetamol");
         medicament1.setPret(10.99);
         medicament1.afiseazaDetalii();
+        medicament2.setTot("Nurofen", 500, "Amidon:(", 10.99);
+        medicament2.afiseazaDetalii();
+
 
         return 0;
 
 	
 }
-
