b6b8a67b9f26c0f4724dd0d625223d64454bdbe9
onyxpeek <onyxpeek@gmail.com>
2023-03-30 13:25:21 +0300

    feat: add the solving for this problem

--- First year/Semester 2/classExam.cpp
+++ First year/Semester 2/classExam.cpp
@@ -3,49 +3,58 @@
 #include <string>
 using namespace std;
 
-class Medicam{
-//Aici am adaugat/definit 4 variabile/atribute de tip privat
-private: 
-	string nume;
-	int concentr = 0;
-	string subsAct;
-	double pret = 0.0;
+
+class Medicament {
+
+private://Avem 4 variabile membre private
+    string nume;
+    int concentr;
+    string subsAct;
+    double pret;
+
 public:
-	void setNume(string numeMedicament){
-		nume = numeMedicament;
-	}
+    //In public avem 5 metode publice
+    void setNume(string numeMedicament) {
+        nume = numeMedicament;
+    }
 
-	void setConcentr(int concentratie){
-		concentr = concentratie;
-	}
+    void setConcentr(int concentratie) {
+        concentr = concentratie;
+    }
 
-	void setSubAct(string substabtaActiva){
-		subsAct = substabtaActiva;
-	}
+    void setSubsAct(string substantaActiva) {
+        subsAct = substantaActiva;
+    }
 
-	void setPret(double pretMedicament){
-		pret = pretMedicament;
-	}
+    void setPret(double pretMedicament) {
+        pret = pretMedicament;
+    }
 
-	void afiseazaDetalii() const{
-		cout << " Nume medicament: " << nume << endl;
-		cout << " Concentratie " << concentr << endl;
+    void afiseazaDetalii() const {
+        cout << "Nume medicament: " << nume << endl;
+        cout << "Concentratie: " << concentr << endl;
         cout << "Substanta activa: " << subsAct << endl;
-        cout << "Pret: " << pret << end;
-	}
+        cout << "Pret: " << pret << endl;
+    }
+
+};
 
 
-}
 
 int main() {
-    Medicament medicament1;
-    medicament1.setNume("Paraacetamol");
-    medicament1.setConcentr(500);
-    medicament1.setSubsAct("Paracetamol");
-    medicament1.setPret(10.99);
-    medicament1.afiseazaDetalii();
+/*O clasa este un tip de date definite de utilizator care contin date si
+metode(datele sunt private si metodele sunt publice), 
+mai precis o metoda este o functie care poate face ceva anumit cu obiectul dat*/
 
-    return 0;
+        Medicament medicament1;
+        medicament1.setNume("Paracetamol");
+        medicament1.setConcentr(500);
+        medicament1.setSubsAct("Paracetamol");
+        medicament1.setPret(10.99);
+        medicament1.afiseazaDetalii();
+
+        return 0;
+
+	
 }
-
 
