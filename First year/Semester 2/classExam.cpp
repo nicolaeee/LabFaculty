1403878f983fa466944f12b16f95f80ce009603d
onyxgeek <onyxpeek@gmail.com>
2023-04-22 11:57:16 +0300

    feat: add new topics on the summer exam

--- First year/Semester 2/classExam.cpp
+++ First year/Semester 2/classExam.cpp
@@ -1,19 +1,24 @@
-//Aici conectez directivele si bibliotecele pentru execunarea corecta a programului cpp
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
-
 class Medicament {
-
-private://Avem 4 variabile membre private
-    string nume;
-    int concentr;
-    string subsAct;
-    double pret;
-
 public:
-    //In public avem 5 metode publice pentru setarea datelor in obiectul medicament 1 prin metoda 1
+    Medicament() {
+        nume = "";
+        concentr = 0;
+        subsAct = "";
+        pret = 0.0;
+    }
+
+    Medicament(string numeMedicament, int concentratie, string substantaActiva, double pretMedicament) {
+        nume = numeMedicament;
+        concentr = concentratie;
+        subsAct = substantaActiva;
+        pret = pretMedicament;
+    }
+
     void setNume(string numeMedicament) {
         nume = numeMedicament;
     }
@@ -30,41 +35,26 @@ public:
         pret = pretMedicament;
     }
 
-    //Aici am o metoda care imi afiseaza aceste date de la obiectul pe care am sa l selectez prin .metoda
-    void afiseazaDetalii()  {
+    void afiseazaDetalii() const {
         cout << "Nume medicament: " << nume << endl;
-        cout << "Connncentratie: " << concentr << endl;
+        cout << "Concentratie: " << concentr << endl;
         cout << "Substanta activa: " << subsAct << endl;
         cout << "Pret: " << pret << endl;
     }
 
-    void setTot(string numeMedicament, int concentratie, string substantaActiva, double pretMedicament) {
-        nume = numeMedicament;
-        concentr = concentratie;
-        subsAct = substantaActiva;
-        pret = pretMedicament;
-    }
-
+private:
+    string nume;
+    int concentr;
+    string subsAct;
+    double pret;
 };
 
-
-
 int main() {
-/*O clasa este un tip de date definite de utilizator care contin date si
-metode(datele sunt private si metodele sunt publice), 
-mai precis o metoda este o functie care poate face ceva anumit cu obiectul dat*/
+    Medicament a1("Aspirina", 500, "acetisalicilc", 10.5);
+    a1.afiseazaDetalii();
 
-        Medicament medicament1, medicament2;
-        medicament1.setNume("Paracetamol");
-        medicament1.setConcentr(500);
-        medicament1.setSubsAct("Pasracetamol");
-        medicament1.setPret(10.99);
-        medicament1.afiseazaDetalii();
-        medicament2.setTot("Nurofen", 500, "Amidon:(", 10.99);
-        medicament2.afiseazaDetalii();
-
-
-        return 0;
-
-	
+    cout << "\n";
+    Medicament a2 = a1;
+    a2.afiseazaDetalii();
+    return 0;
 }
